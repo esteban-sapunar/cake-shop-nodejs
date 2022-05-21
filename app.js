38885c148e40e0e9088c624659e45934cdfef49c
Esteban Sapunar <e_sapu91@hotmail.com>
2022-05-21 18:45:40 -0400

    User Registration and Controller +image and mail

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
+//Require Routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
+// connect database
+mongoose.connect('mongodb+srv://sapu:0728@cakeshop1.rjcmt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+	useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('connected')
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,6 +34,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//configure passport and sessions
+app.use(session({
+  secret: 'hang ten dude',
+  resave: false,
+  saveUninitialized: true
+}));
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
