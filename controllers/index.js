81a4e520c7a3be773ff0bdb81b877a56eb784ae0
Esteban Sapunar <e_sapu91@hotmail.com>
2022-05-21 20:21:47 -0400

    Login and Logout Controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,29 @@
 const User = require('../models/user');
+const passport = require('passport');
 
 module.exports = {
-	postRegister(req, res, next){
+	//Post Register
+	async postRegister(req, res, next){
 		const newUser = new User({
 			username: req.body.username,
 			email: req.body.email,
 			image: req.body.image
 		});
-	  User.register(newUser, req.body.password, (err) => {
-	    if (err) {
-	      console.log('error while user register!', err);
-	      return next(err);
-	    }
 
-	    console.log('user registered!');
+	  	await User.register(newUser, req.body.password);
+	 	res.redirect('/');
 
-	    res.redirect('/');
-	  });
-
+	},
+	//Post Login
+	postLogin(req,res,next){
+		passport.authenticate('local',{
+		  successRedirect: '/',
+		  failureReridect: '/login' 
+		})(req,res,next);
+	},
+	//Get Logout
+	getLogout(req,res,next){
+  		req.logout();
+  		res.redirect('/');
 	}
 }
